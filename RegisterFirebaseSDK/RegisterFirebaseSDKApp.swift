4a9c46d2442dfe3210d3a03cc7a42a5002770270
Alexander <tsurkov53@gmail.com>
2026-03-16 22:38:36 +0300

    Initial Commit

---
+++ RegisterFirebaseSDK/RegisterFirebaseSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterFirebaseSDKApp.swift
+//  RegisterFirebaseSDK
+//
+//  Created by Александр on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RegisterFirebaseSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RegisterFirebaseSDKTests/RegisterFirebaseSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterFirebaseSDKTests.swift
+//  RegisterFirebaseSDKTests
+//
+//  Created by Александр on 16.03.2026.
+//
+
+import Testing
+@testable import RegisterFirebaseSDK
+
+struct RegisterFirebaseSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
